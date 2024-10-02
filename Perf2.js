c1e43dd66b3437b96c4bcfa831310dc1f9116e85
MayRosario12 <162092946+MayRosario12@users.noreply.github.com>
2024-10-02 23:18:55 +0800

    Add files via upload

---
+++ Perf2.js
@@ -0,0 +1,43 @@
+let age = prompt("Enter your age:")
+if (age< 13){
+    console.log("You are a child.");
+ }else if (age >= 13 && age <= 19){
+    console.log("You are a teenager.");
+ }else{
+    console.log("You are an adult.");
+ }
+
+ // Get user input for the day of the week
+ const dayOfWeek =prompt("Enter a day of the week:");
+
+ // Use  a switch statement to map the day to a specific task
+ switch(dayOfWeek.toLowerCas()
+){
+    case "monday":
+        console.log("Your task for Monday is: Finish your assignment.");
+        break; case "tuesday";
+        console.log("You task for Tuesday is: Prepare for the meeting.");
+        break; case "wednesday":
+        console.("You task for Wednesday is: Review  the project plan.");
+        break; case "thursday":
+        console.("You task for Thursday is: Complete  the presentation.");
+        break; case "friday":
+        console.("You task for Friday is: Submit  the report.");
+        break; case "saturday":
+        console.("You task for Saturday is: Relax and recharge.");
+        break; case "sunday":
+        console.("You task for Sunday is: Plan for the upcoming week.");
+        break; default:
+        console("lnvalid day of the week.Please enter a valid day.");
+   }       
+    
+   let age = prompt("Enter your age:")
+if (age< 13){
+    console.log("You are a child.");
+ }else if (age >= 13 && age <= 19){
+    console.log("You are a teenager.");
+ }else{
+    console.log("You are an adult.");
+ }
+
+    
